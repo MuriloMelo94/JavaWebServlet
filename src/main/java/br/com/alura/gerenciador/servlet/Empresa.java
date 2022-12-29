ae3590076d0326c23bc0cba28019883c6d54e5db
MuriloMelo94 <murilomelo94@outlook.com>
2022-12-29 12:14:36 -0300

    Build: :construction: criada a classe empresa.

---
+++ src/main/java/br/com/alura/gerenciador/servlet/Empresa.java
@@ -0,0 +1,24 @@
+package br.com.alura.gerenciador.servlet;
+
+public class Empresa {
+
+	private Integer id;
+	private String nome;
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+}
